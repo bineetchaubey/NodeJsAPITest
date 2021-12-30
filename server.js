feac39c3c7cc4a16f142a19f63cb29dd7e6aa39e
Bineet Kumar Chaubey <bineet08@gmail.com>
2021-12-30 22:18:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,43 @@ app.use(function(req, res, next) {
     next()
 })
 
+
+const agent_list = [
+    {
+        name: 'agent 01',
+        email : 'agent01@mailinator.com',
+        id: 1
+    },
+    {
+        name: 'agent 02',
+        email : 'agent02@mailinator.com',
+        id: 2
+    },
+    {
+        name: 'agent 03',
+        email : 'agent03@mailinator.com',
+        id: 3
+    }
+
+]
+
+
+
 app.get('/getData', function(req, res, next) {
 
     return res.send({
         organization:'Appypie llp',
         age:35,
         name: 'Bineet Kumar Chaubey'
+    })
+})
+
+app.post('/addAgent', function(req, res, next) {
+
+    agent_list.push(req.body)
+    return res.send({
+        success: true,
+        result: agent_list
     })
 })
 
