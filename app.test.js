2e23c914a8cb7c7595b876d5acacde41db5d247a
Bineet Kumar Chaubey <bineet08@gmail.com>
2021-12-30 21:52:24 +0530

    update test cases

--- app.test.js
+++ app.test.js
@@ -1,7 +1,9 @@
 const chai = require('chai');
 const server = require('../server.js')
 const  chaiHttp = require('chai-http')
+const expect = require('chai').expect
 chai.should();
+chai.expect()
 chai.use(chaiHttp);
 
 describe('Test Node js API ',()=>{
@@ -22,12 +24,21 @@ describe('Test Node js API ',()=>{
                 end((err,response) => {
                     // console.log(response)
                     // response.should.have.status(200);
-                    // response.body.should.have.property('name')
+                    response.body.should.be.a('Object')
                     response.body.should.have.property('age').be.equal(35)
+                    response.body.should.have.property('name').be.equal('Bineet Kumar Chaubey')
                     response.body.should.have.property('age').and.to.be.a('Number')
                 })
-
             })
+
+            it('BDD test  ',() =>{
+                chai.request(server).get('/getData').
+                end((err,response) => {
+                    expect(response.body).to.have.keys('age', 'name','organization');
+                    
+                })
+            })
+           
     })
 
 }) 
